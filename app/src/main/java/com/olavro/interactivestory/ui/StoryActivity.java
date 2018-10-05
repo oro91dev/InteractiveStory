dadab39f185d357117443174f0df580299f0c627
OroDev <oro91dev@gmail.com>
2018-10-05 14:40:39 +0200

    Added portrait orientation and color for ui elements

--- app/src/main/java/com/olavro/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/olavro/interactivestory/ui/StoryActivity.java
@@ -6,12 +6,15 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.olavro.interactivestory.R;
 import com.olavro.interactivestory.model.Page;
 import com.olavro.interactivestory.model.Story;
+
+import java.util.Stack;
 
 public class StoryActivity extends AppCompatActivity {
 
@@ -23,7 +26,7 @@ public class StoryActivity extends AppCompatActivity {
     private TextView storyTextView;
     private Button choice1Button;
     private Button choice2Button;
-    private Page[] pages;
+    private Stack<Integer> pageStack = new Stack<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +48,66 @@ public class StoryActivity extends AppCompatActivity {
 
         story = new Story();
         loadPage(0);
-        pages = new Page[7];
     }
 
     private void loadPage(int pageNumber) {
-        Page page = story.getPage(pageNumber);
+        pageStack.push(pageNumber);
+        final Page page = story.getPage(pageNumber);
 
         Drawable image = ContextCompat.getDrawable(this, page.getImageId());
         storyImageView.setImageDrawable(image);
 
         String pageText = getString(page.getTextId());
+        // Add name if placeholder included. Won't add if not
         pageText = String.format(pageText, name);
         storyTextView.setText(pageText);
 
+        if (page.isFinalPage()) {
+            choice1Button.setVisibility(View.INVISIBLE);
+            choice2Button.setText(R.string.play_again_button_text);
+            choice2Button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loadPage(0);
+                }
+            });
+        }
+        else {
+            loadButtons(page);
+        }
+    }
+
+    private void loadButtons(final Page page) {
+        choice1Button.setVisibility(View.VISIBLE);
         choice1Button.setText(page.getChoice1().getTextId());
+        choice1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nextPage = page.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+
+        choice2Button.setVisibility(View.VISIBLE);
         choice2Button.setText(page.getChoice2().getTextId());
+        choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        pageStack.pop();
+        if (pageStack.isEmpty()) {
+            super.onBackPressed();
+        }
+        else {
+            loadPage(pageStack.pop());
+        }
+        super.onBackPressed();
     }
 }
